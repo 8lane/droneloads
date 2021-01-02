1249a384de294f89973a3823517806d39318fd16
Tom  Christian <tctc91@gmail.com>
2021-01-02 14:54:33 +0000

    async font

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,19 @@ class MyDocument extends Document {
       <Html lang="en" className="text-coolGray-200 leading-tight">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&amp;display=swap" rel="stylesheet" />
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&amp;display=swap"
+            as="style"
+            onload="this.onload=null;this.rel='stylesheet'"
+          />
+          <noscript>
+            <link
+              href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&amp;display=swap"
+              rel="stylesheet"
+              type="text/css"
+            />
+          </noscript>
         </Head>
         <body className="min-h-screen bg-coolGray-900">
           <Main />
