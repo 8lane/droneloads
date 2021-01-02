a4252b3a55f80911ada15a69fba00f081ddf8ced
Tom  Christian <tctc91@gmail.com>
2021-01-02 14:57:57 +0000

    a11y

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
           href="https://tomchristian.co.uk"
           target="_blank"
           rel="noopener"
-          className='text-sm transition-colors text-coolGray-500 hover:text-coolGray-300'
+          className='text-sm transition-colors text-coolGray-400 hover:text-coolGray-300'
         >
           Project by Tom Christian
         </a>
